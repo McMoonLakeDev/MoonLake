e948ad7c5a48aeaddc27b4be1e2575bb8ee1e089
McMoonLake <ablu2g@sina.com>
2016-04-26 16:53:51 +0800

    init

---
+++ src/com/minecraft/moonlake/MoonLakePlugin.java
@@ -0,0 +1,17 @@
+package com.minecraft.moonlake;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by MoonLake on 2016/4/26.
+ * @version 1.0
+ * @author Month_Light
+ */
+public class MoonLakePlugin extends JavaPlugin {
+
+    static {}
+
+    public MoonLakePlugin() {
+        // 构造函数
+    }
+}
