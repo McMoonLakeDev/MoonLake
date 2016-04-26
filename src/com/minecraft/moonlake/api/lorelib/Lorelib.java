990cd4f77284148733a89298ffbf86739972e6ef
McMoonLake <ablu2g@sina.com>
2016-04-27 00:03:36 +0800

    Lore lib

--- src/com/minecraft/moonlake/api/lorelib/Lorelib.java
+++ src/com/minecraft/moonlake/api/lorelib/Lorelib.java
@@ -21,6 +21,7 @@ public interface Lorelib {
 
     /**
      * 给物品栈设置标签
+     *
      * @param item 物品栈
      * @param lore 标签
      * @return ItemStack
@@ -29,6 +30,7 @@ public interface Lorelib {
 
     /**
      * 给物品栈设置标签
+     *
      * @param item 物品栈
      * @param lore 标签
      * @return ItemStack
@@ -52,4 +54,31 @@ public interface Lorelib {
      * @return ItemStack
      */
     ItemStack addLore(ItemStack item, List<String> lore);
+
+    /**
+     * 获取物品栈是否拥有标签
+     *
+     * @param item 物品栈
+     * @return 是否拥有标签
+     */
+    boolean hasLore(ItemStack item);
+
+    /**
+     * 获取物品栈指定索引的标签
+     *
+     * @param item 物品栈
+     * @param index 索引
+     * @return 索引标签 如果物品栈没有标签或索引越界则返回空
+     */
+    String getLoreFromIndex(ItemStack item, int index);
+
+    /**
+     * 给物品栈在指定索引插入标签
+     *
+     * @param item 物品栈
+     * @param index 索引
+     * @param insertLore 插入的标签
+     * @return 插入标签后的 ItemStack 如果索引越界则追加标签
+     */
+    ItemStack insertLoreFromIndex(ItemStack item, int index, String... insertLore);
 }
--- src/com/minecraft/moonlake/util/lore/LoreUtil.java
+++ src/com/minecraft/moonlake/util/lore/LoreUtil.java
@@ -5,6 +5,7 @@ import com.minecraft.moonlake.util.Util;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -108,5 +109,57 @@ public class LoreUtil implements Lorelib {
             temp.add(Util.color(lore.get(i)));
         }
         return setLore(item, temp);
+    }
+
+    /**
+     * 获取物品栈是否拥有标签
+     *
+     * @param item 物品栈
+     * @return 是否拥有标签
+     */
+    @Override
+    public boolean hasLore(ItemStack item) {
+        return getLore(item) != null;
+    }
+
+    /**
+     * 获取物品栈指定索引的标签
+     *
+     * @param item 物品栈
+     * @param index 索引
+     * @return 索引标签 如果物品栈没有标签或索引越界则返回空
+     */
+    @Override
+    public String getLoreFromIndex(ItemStack item, int index) {
+        List<String> temp = getLore(item);
+        return temp != null && temp.size() - 1 <= index ? temp.get(index) : "";
+    }
+
+    /**
+     * 给物品栈在指定索引插入标签
+     *
+     * @param item 物品栈
+     * @param index 索引
+     * @param insertLore 插入的标签
+     * @return 插入标签后的 ItemStack 如果索引越界则追加标签
+     */
+    @Override
+    public ItemStack insertLoreFromIndex(ItemStack item, int index, String... insertLore) {
+        List<String> temp = getLore(item);
+        if(temp == null) {
+            return setLore(item, insertLore);
+        }
+        if(index >= temp.size() - 1) {
+            return addLore(item, insertLore);
+        }
+        List<String> lore = new ArrayList<String>();
+        for(int i = 0; i < index + 1; i++) {
+            lore.add(temp.get(i));
+        }
+        lore.addAll(Arrays.asList(Util.color(insertLore)));
+        for(int i = 0;i < temp.size() - index + 1; i++) {
+            lore.add(temp.get(i));
+        }
+        return setLore(item, lore);
     }
 }
