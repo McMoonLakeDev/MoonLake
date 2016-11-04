f0b2b5c02c5cbc87bcb17719ab38493b4d0c607e
u2g <mcmoonlake@hotmail.com>
2016-11-04 13:14:03 +0800

    更新到 v1.8-a3 版本

--- src/com/minecraft/moonlake/MoonLakePlugin.java
+++ src/com/minecraft/moonlake/MoonLakePlugin.java
@@ -33,7 +33,7 @@ import java.util.Set;
  * <hr />
  * <div>
  *     <h1>Minecraft MoonLake Core API Plugin</h1>
- *     <p>By Month_Light Ver: 1.8-r2</p>
+ *     <p>By Month_Light Ver: 1.8-a3</p>
  *     <p>Website: <a href="http://www.mcyszh.com" target="_blank" style="text-decoration: none;">MoonLake Website</a></p>
  *     <p>QQ Group: 377607025 -> <a href="http://jq.qq.com/?_wv=1027&k=2IfPFrH" target="_blank">Jump</a></p>
  *     <hr />
@@ -68,7 +68,7 @@ import java.util.Set;
  *     <h1>修改操作请您遵守 <a href="https://github.com/u2g/MoonLake/blob/master/LICENSE" target="_blank">GPLv3</a> 协议，您必须公开修改过的所有代码！</h1>
  * </div>
  *
- * @version 1.8-r2
+ * @version 1.8-a3
  * @author Month_Light
  */
 public class MoonLakePlugin extends JavaPlugin implements MoonLake {
