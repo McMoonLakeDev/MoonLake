53b90a4c94ead78c51b19f591e49d3a2ae4839c9
u2g <mcmoonlake@hotmail.com>
2017-01-09 12:46:34 +0800

    实体管理类的 getMaxHealth 过期函数移除

--- src/com/minecraft/moonlake/manager/EntityManager.java
+++ src/com/minecraft/moonlake/manager/EntityManager.java
@@ -551,7 +551,7 @@ public class EntityManager extends MoonLakeManager {
 
         if(regain && entity instanceof LivingEntity) {
 
-            ((LivingEntity)entity).setHealth(((LivingEntity)entity).getMaxHealth()); // TODO
+            ((LivingEntity)entity).setHealth(value);
         }
     }
 
