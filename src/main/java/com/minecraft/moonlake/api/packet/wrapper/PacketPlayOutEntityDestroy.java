0309f730771bfa3cf60952f6a514c8bc08aed626
u2g <mcmoonlake@hotmail.com>
2017-02-08 18:51:08 +0800

    数据包 PacketPlayOutEntityDestroy 新版重做基础完成

--- src/main/java/com/minecraft/moonlake/api/packet/wrapper/PacketPlayOutEntityDestroy.java
+++ src/main/java/com/minecraft/moonlake/api/packet/wrapper/PacketPlayOutEntityDestroy.java
@@ -18,22 +18,93 @@
  
 package com.minecraft.moonlake.api.packet.wrapper;
 
+import com.minecraft.moonlake.api.packet.exception.PacketInitializeException;
+import com.minecraft.moonlake.property.ObjectProperty;
+import com.minecraft.moonlake.property.SimpleObjectProperty;
+import com.minecraft.moonlake.validate.Validate;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+
+import static com.minecraft.moonlake.reflect.Reflect.*;
 
 /**
  * <h1>PacketPlayOutEntityDestroy</h1>
  * 数据包输出实体破坏（详细doc待补充...）
  *
- * @version 1.0
+ * @version 2.0
  * @author Month_Light
  */
 public class PacketPlayOutEntityDestroy extends PacketPlayOutBukkitAbstract {
 
+    private final static Class<?> CLASS_PACKETPLAYOUTENTITYDESTROY;
+
+    static {
+
+        try {
+
+            CLASS_PACKETPLAYOUTENTITYDESTROY = PackageType.MINECRAFT_SERVER.getClass("PacketPlayOutEntityDestroy");
+        }
+        catch (Exception e) {
+
+            throw new PacketInitializeException("The net.minecraft.server packet play out entity destroy reflect raw initialize exception.", e);
+        }
+    }
+
+    private ObjectProperty<int[]> entityIds;
+
     public PacketPlayOutEntityDestroy() {
+
+        this(new int[0]);
+    }
+
+    public PacketPlayOutEntityDestroy(Entity... entities) {
+
+        int index = 0;
+        int[] entityIds = new int[entities.length];
+
+        for(Entity entity : entities)
+            entityIds[index++] = entity.getEntityId();
+
+        this.entityIds = new SimpleObjectProperty<>(entityIds);
+    }
+
+    public PacketPlayOutEntityDestroy(int... entityIds) {
+
+        this.entityIds = new SimpleObjectProperty<>(entityIds);
+    }
+
+    public ObjectProperty<int[]> entityIdsProperty() {
+
+        return entityIds;
     }
 
     @Override
     protected boolean sendPacket(Player... players) throws Exception {
+
+        int[] entityIds = entityIdsProperty().get();
+        Validate.notNull(entityIds, "The entity id object is null.");
+
+        try {
+            // 先用调用 NMS 的 PacketPlayOutEntityDestroy 构造函数, 参数 int[]
+            // 进行反射实例发送
+            sendPacket(players, instantiateObject(CLASS_PACKETPLAYOUTENTITYDESTROY, entityIds));
+            return true;
+
+        } catch (Exception e) {
+            // 如果异常了说明 NMS 的 PacketPlayOutEntityDestroy 构造函数不存在这个参数类型
+            // 那么用反射直接设置字段值方式来发送
+            try {
+                // 判断字段数量等于 1 个的话就是有此方式
+                // 这个字段对应 int[] 属性
+                Object packet = instantiateObject(CLASS_PACKETPLAYOUTENTITYDESTROY);
+                Object[] values = { entityIds };
+                setFieldAccessibleAndValueSend(players, 1, CLASS_PACKETPLAYOUTENTITYDESTROY, packet, values);
+                return true;
+
+            } catch (Exception e1) {
+            }
+        }
+        // 否则前面的方式均不支持则返回 false 并抛出不支持运算异常
         return false;
     }
 }
