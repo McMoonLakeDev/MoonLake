78bc2daa861f4a4e027e79db651382a9a7b4cdf3
MoonLake <mcmoonlake@hotmail.com>
2017-09-10 18:06:01 +0800

    [Update] 附魔枚举的排版调整，太过于强迫症了 2333

--- API/src/main/kotlin/com/minecraft/moonlake/api/item/Enchantment.kt
+++ API/src/main/kotlin/com/minecraft/moonlake/api/item/Enchantment.kt
@@ -21,41 +21,41 @@ import com.minecraft.moonlake.api.version.MinecraftVersion
 
 enum class Enchantment(val id: Int, val max: Int, val mcVer: MinecraftVersion? = null) {
 
-    PROTECTION(0, 4),                                                                                 保护(PROTECTION),
-    PROTECTION_FIRE(1, 4),                                                                     火焰保护(PROTECTION_FIRE),
-    PROTECTION_FALL(2, 4),                                                                    摔落保护(PROTECTION_FALL),
-    PROTECTION_EXPLOSIONS(3, 4),                                                    爆炸保护(PROTECTION_EXPLOSIONS),
-    PROTECTION_PROJECTILE(4, 4),                                                    弹射物保护(PROTECTION_PROJECTILE),
-    OXYGEN(5, 3),                                                                                           水下呼吸(OXYGEN),
-    WATER_WORKER(6, 1),                                                                        水下速倔(WATER_WORKER),
-    THORNS(7, 3),                                                                                           荆棘(THORNS),
-    DEPTH_STRIDER(8, 3),                                                                           深海探索者(DEPTH_STRIDER),
+    PROTECTION(0, 4),                                                                  保护(PROTECTION),
+    PROTECTION_FIRE(1, 4),                                                          火焰保护(PROTECTION_FIRE),
+    PROTECTION_FALL(2, 4),                                                         摔落保护(PROTECTION_FALL),
+    PROTECTION_EXPLOSIONS(3, 4),                                            爆炸保护(PROTECTION_EXPLOSIONS),
+    PROTECTION_PROJECTILE(4, 4),                                              弹射物保护(PROTECTION_PROJECTILE),
+    OXYGEN(5, 3),                                                                         水下呼吸(OXYGEN),
+    WATER_WORKER(6, 1),                                                            水下速倔(WATER_WORKER),
+    THORNS(7, 3),                                                                         荆棘(THORNS),
+    DEPTH_STRIDER(8, 3),                                                              深海探索者(DEPTH_STRIDER),
     FROST_WALKER(9, 2, MinecraftVersion.V1_9),                         冰霜行者(FROST_WALKER),
     BINDING_CURSE(10, 1, MinecraftVersion.V1_11),                   绑定诅咒(BINDING_CURSE),
 
-    DAMAGE(16, 5),                                                                                       锋利(DAMAGE),
-    DAMAGE_UNDEAD(17, 5),                                                                  亡灵杀手(DAMAGE_UNDEAD),
-    DAMAGE_ARTHROPODS(18, 5),                                                      节肢杀手(DAMAGE_ARTHROPODS),
-    KNOCKBACK(19, 2),                                                                              击退(KNOCKBACK),
-    FIRE_ASPECT(20, 2),                                                                              火焰附加(FIRE_ASPECT),
-    LOOT_BONUS_MOBS(21, 3),                                                             抢夺(LOOT_BONUS_MOBS),
-    SWEEPING_EDGE(22, 3, MinecraftVersion(1, 11, 1)),              横扫之刃(SWEEPING_EDGE),
+    DAMAGE(16, 5),                                                                      锋利(DAMAGE),
+    DAMAGE_UNDEAD(17, 5),                                                      亡灵杀手(DAMAGE_UNDEAD),
+    DAMAGE_ARTHROPODS(18, 5),                                              节肢杀手(DAMAGE_ARTHROPODS),
+    KNOCKBACK(19, 2),                                                                击退(KNOCKBACK),
+    FIRE_ASPECT(20, 2),                                                                 火焰附加(FIRE_ASPECT),
+    LOOT_BONUS_MOBS(21, 3),                                                   抢夺(LOOT_BONUS_MOBS),
+    SWEEPING_EDGE(22, 3, MinecraftVersion(1, 11, 1)),                横扫之刃(SWEEPING_EDGE),
 
-    DIG_SPEED(32, 5),                                                                                  效率(DIG_SPEED),
-    SILK_TOUCH(33, 1),                                                                               精准采集(SILK_TOUCH),
-    DURABILITY(34, 3),                                                                                 耐久(DURABILITY),
-    LOOT_BONUS_BLOCKS(35, 3),                                                        时运(LOOT_BONUS_BLOCKS),
+    DIG_SPEED(32, 5),                                                                   效率(DIG_SPEED),
+    SILK_TOUCH(33, 1),                                                                 精准采集(SILK_TOUCH),
+    DURABILITY(34, 3),                                                                  耐久(DURABILITY),
+    LOOT_BONUS_BLOCKS(35, 3),                                                时运(LOOT_BONUS_BLOCKS),
 
-    ARROW_DAMAGE(48, 5),                                                                   力量(ARROW_DAMAGE),
-    ARROW_KNOCKBACK(49, 2),                                                          冲击(ARROW_KNOCKBACK),
-    ARROW_FIRE(50, 1),                                                                             火矢(ARROW_FIRE),
-    ARROW_INFINITE(51, 1),                                                                     无限(ARROW_INFINITE),
+    ARROW_DAMAGE(48, 5),                                                         力量(ARROW_DAMAGE),
+    ARROW_KNOCKBACK(49, 2),                                                   冲击(ARROW_KNOCKBACK),
+    ARROW_FIRE(50, 1),                                                                 火矢(ARROW_FIRE),
+    ARROW_INFINITE(51, 1),                                                          无限(ARROW_INFINITE),
 
-    LUCK(61, 3),                                                                                              海至眷顾(LUCK),
-    LURE(62, 3),                                                                                              饵钓(LURE),
+    LUCK(61, 3),                                                                             海至眷顾(LUCK),
+    LURE(62, 3),                                                                              饵钓(LURE),
 
-    MENDING(70, 1, MinecraftVersion.V1_9),                                  经验修补(MENDING),
-    VANISHING_CURSE(71, 1, MinecraftVersion.V1_11),            消失诅咒(VANISHING_CURSE),
+    MENDING(70, 1, MinecraftVersion.V1_9),                                 经验修补(MENDING),
+    VANISHING_CURSE(71, 1, MinecraftVersion.V1_11),            	消失诅咒(VANISHING_CURSE),
     ;
 
     constructor(equivalent: Enchantment) : this(equivalent.id, equivalent.max, equivalent.mcVer)
@@ -64,6 +64,6 @@ enum class Enchantment(val id: Int, val max: Int, val mcVer: MinecraftVersion? =
      * @throws IllegalArgumentException if the level is unsafe
      */
     @Throws(IllegalArgumentException::class)
-    fun checkSafe(level: Int)
+    fun checkSafe(level: Int) // TODO check item enchantment support
             = if(level < 1 || level > max) throw IllegalArgumentException() else Unit
 }
